0f0d998cf0cc63727715bf2f96cea193fb277f2f
charon-cheung <zhangzipan@protonmail.com>
2020-05-26 16:58:16 +0800

    add gmapping

---
+++ gmapping/src/main.cpp
@@ -0,0 +1,18 @@
+/* Author: Brian Gerkey */
+
+#include <ros/ros.h>
+
+#include "slam_gmapping.h"
+
+int
+main(int argc, char** argv)
+{
+  ros::init(argc, argv, "slam_gmapping");
+
+  SlamGMapping gn;
+  gn.startLiveSlam();
+  ros::spin();
+
+  return(0);
+}
+
